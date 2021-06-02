47637b969e8e7739bfabb6b3c7092ce2203efe88
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-06-02 21:07:20 +0200

    add paging controller

---
+++ CatsPicturesApp/Presentation/Helpers/PagingController.swift
@@ -0,0 +1,48 @@
+//
+//  PagingController.swift
+//  CatsPicturesApp
+//
+//  Created by Ahmed Ali on 02/06/2021.
+//
+
+import Foundation
+// MARK: - PagingController
+//
+class PagingController {
+    // MARK: - Properties
+    //
+    private var totalNumberOfPages: Int = .max
+    private var isLoading = false
+
+    private var hasReachedLastPage: Bool {
+        return nextPageIndex > totalNumberOfPages
+    }
+
+    /// The index of the next page to load.
+    private(set) var nextPageIndex: Int = 1
+
+    /// Check if client should load next page, returns false if loading is already in progress
+    /// or client have already reached the last page.
+    var shouldLoadNextPage: Bool {
+        isLoading == false && hasReachedLastPage == false
+    }
+
+    /// Notify controller that you have started loading next page.
+    func startLoadingNextPage() {
+        isLoading = true
+    }
+
+    /// Notif that controller that loading page has ended with information
+    /// about what's loaded
+    ///
+    func loadedPage() {
+        nextPageIndex += 1
+        isLoading = false
+    }
+
+    /// Notify the controller that loading has finished but no pages were loaded
+    ///
+    func finishedWithError() {
+        isLoading = false
+    }
+}
