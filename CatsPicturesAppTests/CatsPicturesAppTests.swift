f5f4659f4db80ecfd8a453d285dac2ba7c114cc8
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-06-02 11:31:02 +0200

    Init our iOS Project 🚀

--- CatsPicturesAppTests/CatsPicturesAppTests.swift
+++ CatsPicturesAppTests/CatsPicturesAppTests.swift
@@ -12,9 +12,12 @@ class CatsPicturesAppTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        try? super.setUpWithError()
     }
 
     override func tearDownWithError() throws {
+        try? super.tearDownWithError()
+
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
