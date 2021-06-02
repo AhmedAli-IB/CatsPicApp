d9395c6483c9d2f1b6b62f75d0e104b79eb43547
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-06-02 13:46:19 +0200

    Add cats response entity

---
+++ CatsPicturesApp/Domain/Entities/CatResponse.swift
@@ -0,0 +1,14 @@
+//
+//  CatResponse.swift
+//  CatsPicturesApp
+//
+//  Created by Ahmed Ali on 02/06/2021.
+//
+
+import Foundation
+
+// MARK: - CatsResponse
+//
+struct CatsResponse: Codable {
+    let url: String
+}
