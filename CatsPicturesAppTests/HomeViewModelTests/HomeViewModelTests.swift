fd6cf4a3ad2cb4735a0c4f3f3248df0fc1f40994
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-06-03 15:07:16 +0200

    Add unit test for home data view model

---
+++ CatsPicturesAppTests/HomeViewModelTests/HomeViewModelTests.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModelTests.swift
+//  CatsPicturesAppTests
+//
+//  Created by Ahmed Ali on 03/06/2021.
+//
+
+import Foundation
---
+++ CatsPicturesAppTests/HomeViewModelTests/PhotoUseCaseMock.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoUseCaseMock.swift
+//  CatsPicturesAppTests
+//
+//  Created by Ahmed Ali on 03/06/2021.
+//
+
+import Foundation
