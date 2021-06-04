a34b55fc629380c44bb2a7f856d4a28cd680d2ff
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-06-04 16:09:15 +0200

    Add nsnotification helpers

---
+++ CatsPicturesApp/Extensions/NotificationCenter+Helpers.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationCenter+Helpers.swift
+//  CatsPicturesApp
+//
+//  Created by Ahmed Ali on 04/06/2021.
+//
+
+import Foundation
+// MARK: - MainTabViewModel Notifications
+//
+extension NSNotification.Name {
+    static let homeListReloadRequired = Foundation.Notification.Name(rawValue: "homeListReloadRequired")
+}
